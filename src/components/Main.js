0169162d70c2b13e7282c3e9e42fced5ef0863cb
Marie Lenac <lenacm02@gmail.com>
2020-05-08 21:38:02 -0700

    ready for demo

---
+++ src/components/Main.js
@@ -0,0 +1,12 @@
+import React from "feact";
+import "./style.css";
+
+const Main = () => {
+  return (
+    <div className="main-area demo">
+      <div className="header-div"></div>
+    </div>
+  );
+};
+
+export default Main;
